c6453fe6e007d6df45b10555fc4a1deb3a5f405a
Pierre Lecointre <pierre.lecointre@sitembe.net>
2014-06-22 20:25:42 +0200

    first commit
    v1.4

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a list containing a function to set and get a matrix and to set and get its inverse, using 
+## the R function solve.
 
-## Write a short comment describing this function
+## pfml, 20140622, v1.4
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <-function(solve) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix created by the above function. it first sees if this inverse
+## has been already computed; in this case it returns the cache value.
+## Otherwise, assuming the matrix is a sqaure inversible matrix, it uses the function solve() to calculate its inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
